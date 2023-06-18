c9717c3be49b88a6fe7513bf45ac4f59c796367f
Mev Developer <126674880+Mevdevoloper@users.noreply.github.com>
2023-06-18 20:15:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,12 @@ from telegram.ext import (
     MessageHandler,
     Updater,
 )
+from pymongo import MongoClient
+from config import MONGO_URI, DB_NAME, COLLECTION_NAME
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+collection = db[COLLECTION_NAME]
 
 TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
 
