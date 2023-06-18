c945ec2f4b33bbd95ce809096e22e4e101576bf8
Mev Developer <126674880+Mevdevoloper@users.noreply.github.com>
2023-06-18 20:13:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,36 @@
+import logging
+from telegram import Update
+from telegram.ext import (
+    CallbackContext,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+    Updater,
+)
+
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Handler for the /start command."""
+    update.message.reply_text("Welcome to the Image to PDF Converter Bot!")
+
+def convert_image_to_pdf(update: Update, context: CallbackContext) -> None:
+    """Handler for converting image to PDF."""
+    # Your conversion logic here
+
+def main() -> None:
+    """Start the bot."""
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.photo, convert_image_to_pdf))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
 
